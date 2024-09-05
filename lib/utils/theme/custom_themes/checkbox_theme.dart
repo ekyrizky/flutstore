a63cdc9f7a61189cfbc2c46bd317f6788e21e9ce
Rizky Ananda <muhammad.ananda@visionplus.id>
2024-09-05 14:00:20 +0700

    add custom theme

---
+++ lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class FCheckboxTheme {
+  FCheckboxTheme._();
+
+  static CheckboxThemeData lightCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: MaterialStateProperty.resolveWith((state) {
+      if (state.contains(MaterialState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: MaterialStateProperty.resolveWith((state) {
+      if (state.contains(MaterialState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+
+  static CheckboxThemeData darkCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: MaterialStateProperty.resolveWith((state) {
+      if (state.contains(MaterialState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: MaterialStateProperty.resolveWith((state) {
+      if (state.contains(MaterialState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+}
---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,45 @@
+import 'package:flutstore/utils/theme/custom_themes/appbar_theme.dart';
+import 'package:flutstore/utils/theme/custom_themes/bottom_sheet_theme.dart';
+import 'package:flutstore/utils/theme/custom_themes/checkbox_theme.dart';
+import 'package:flutstore/utils/theme/custom_themes/chip_theme.dart';
+import 'package:flutstore/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:flutstore/utils/theme/custom_themes/outlined_button_theme.dart';
+import 'package:flutstore/utils/theme/custom_themes/text_field_theme.dart';
+import 'package:flutstore/utils/theme/custom_themes/text_theme.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: FTextTheme.lightTextTheme,
+    elevatedButtonTheme: FElevatedButtonTheme.lightElevatedButton,
+    appBarTheme: FAppBarTheme.lightAppBarTheme,
+    bottomSheetTheme: FBottomSheetTheme.lightBottomSheetTheme,
+    checkboxTheme: FCheckboxTheme.lightCheckboxTheme,
+    chipTheme: FChipTheme.lightChipTheme,
+    outlinedButtonTheme: FOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: FTextFieldTheme.lightInputDecorationTheme,
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: FTextTheme.darkTextTheme,
+    elevatedButtonTheme: FElevatedButtonTheme.darkElevatedButton,
+    appBarTheme: FAppBarTheme.darkAppBarTheme,
+    bottomSheetTheme: FBottomSheetTheme.darkBottomSheetTheme,
+    checkboxTheme: FCheckboxTheme.darkCheckboxTheme,
+    chipTheme: FChipTheme.darkChipTheme,
+    outlinedButtonTheme: FOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: FTextFieldTheme.darkInputDecorationTheme,
+  );
+}
