e4e37481bba7e70edd4f4501f6d55a8520d32e8a
Rizky Ananda <m.rizkyananda57@gmail.com>
2024-10-22 12:41:04 +0700

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutstore/app.dart';
+import 'package:flutstore/firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const App());
 }
